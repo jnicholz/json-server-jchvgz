99f778c6bb58f2b280f81a9dd9e66acb65140949
jnicholz <10808080@my.uvu.edu>
2022-08-25 19:40:05 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-server-jchvgz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/json-server-jchvgz)
---
+++ public/script.js
@@ -0,0 +1,9 @@
+// TODO: Wire up the app's behavior here.
+// NOTE: The TODOs are listed in index.html
+function createUUID() {
+  return 'xxxxxxx'.replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
