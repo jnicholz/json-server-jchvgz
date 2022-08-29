1afa3cda4514f6c05f058f347d7b6d77ac50de34
jnicholz <10808080@my.uvu.edu>
2022-08-29 13:00:28 -0600

    3 files modified

--- public/script.js
+++ public/script.js
@@ -1,59 +1,64 @@
 // TODO: Wire up the app's behavior here.
 // NOTE: The TODOs are listed in index.html
-var courseSel =''
-var courses ={}
-var stat
+var courseSel = '';
+var courses = {};
+var stat;
+var stdID;
 function createUUID() {
   return 'xxxxxxx'.replace(/[xy]/g, function (c) {
     var r = (Math.random() * 16) | 0,
-      v = c == 'x' ? r : (r & 0x3) | 0x8
-    return v.toString(16)
-  })
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 async function getCourses() {
-  console.log('get Courses triggered')
+  console.log('get Courses triggered');
   fetch('https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/courses')
     .then((response) => response.json())
     .then((data) => {
       for (datum in data) {
         document.getElementById(
           'course'
-        ).innerHTML += `<option value=${data[datum].id}>${data[datum].display}</option>`
+        ).innerHTML += `<option value=${data[datum].id}>${data[datum].display}</option>`;
       }
-      courses = data
-    })
+      courses = data;
+    });
 }
 function peekABoo(data) {
   if (data != 0) {
-    document.getElementById('restOfBody').style.display = 'block'
-    courseSel = courses[data].id
+    document.getElementById('restOfBody').style.display = 'block';
+    courseSel = courses[data].id;
   } else {
-    document.getElementById('restOfBody').style.display = 'none'
-    courseSel =''
+    document.getElementById('restOfBody').style.display = 'none';
+    courseSel = '';
   }
 }
 
-async function checkIt(data){
-  console.log(data)
-  if (!isNaN(data)){
-    if (data >9999999){
-      console.log('it be 8 char' )
-      document.getElementById('uvuIdDisplay').innerText = `Student Logs for ${data}`
-      getNotes(data)
+async function checkIt(data) {
+  console.log(data);
+  if (!isNaN(data)) {
+    if (data > 9999999) {
+      console.log('it be 8 char');
+      document.getElementById(
+        'uvuIdDisplay'
+      ).innerText = `Student Logs for ${data}`;
+      stdID = data;
+      getNotes(data);
     }
   }
 }
-async function getNotes(id){
-  console.log('get notes triggered')
-  fetch(`https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/logs?courseId=${courseSel}&uvuId=${id}`)
-    .then((response)=>response.json() 
-    )
+async function getNotes(id) {
+  console.log('get notes triggered');
+  fetch(
+    `https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/logs?courseId=${courseSel}&uvuId=${id}`
+  )
+    .then((response) => response.json())
     .then((data) => {
-      console.log(JSON.stringify(Response))
-        for (datum in data) {
-          document.getElementById(
-            'logsList'
-          ).innerHTML += `<li onclick="toggleIt( '${data[datum].id}' )">
+      console.log(JSON.stringify(Response));
+      for (datum in data) {
+        document.getElementById(
+          'logsList'
+        ).innerHTML += `<li onclick="toggleIt( '${data[datum].id}' )">
             <div>
               <small> 
                 ${data[datum].date}
@@ -61,20 +66,53 @@ async function getNotes(id){
             </div>
             <p id="${data[datum].id}" style="display:block">
               ${data[datum].text}
-            </p></li>`
-        }
-        document.getElementById("submitButton").removeAttribute("disabled")
-      } 
-    )
+            </p></li>`;
+      }
+      document.getElementById('submitButton').removeAttribute('disabled');
+    });
 }
-function toggleIt(data){
-  console.log(data)
-  disp = document.getElementById(data).style.display
-  if (disp == "block"){
-    console.log("at block")
-    document.getElementById(data).style.display = "none"
+function toggleIt(data) {
+  console.log(data);
+  disp = document.getElementById(data).style.display;
+  if (disp == 'block') {
+    console.log('at block');
+    document.getElementById(data).style.display = 'none';
+  } else {
+    document.getElementById(data).style.display = 'block';
   }
-  else{
-    document.getElementById(data).style.display = "block"
+}
+async function sendIt() {
+  let rn = new Date();
+  let idToUse =createUUID()
+  const data = `"courseId": "${courseSel}","uvuId": "${stdID}","date": "${rn.toLocaleString()}","text": "${
+    document.getElementById('logInput').value
+  }","id": ${idToUse}`;
+  console.log(data);
+  try {
+    let response = await fetch(
+      `https://json-server-jchvgz--3000.local.webcontainer.io/api/v1/logs`,
+      {
+        method: 'POST',
+        headers: {
+          'contentType': 'application/json', 
+          "courseId": `"${courseSel}"`,
+          "uvuId": `"${stdID}"`,
+          "date": `"${rn.toLocaleString()}"`,
+          "text": `"${
+            document.getElementById('logInput').value}"`,
+            "id": `"${idToUse}"`,
+        },
+        body: JSON.stringify(data),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  } catch (err) {
+    console.log(err);
   }
 }
